4592bd3718010247035d5f3d3e29f8c0869197a3
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-09-02 16:55:44 +0600

    table view integration

--- Netflix UIKit/Controllers/HomeViewController.swift
+++ Netflix UIKit/Controllers/HomeViewController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    private let homeFeedTableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTableView)
+        
+        homeFeedTableView.delegate = self
+        homeFeedTableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTableView.frame = view.bounds
     }
-    */
 
+}
+
+
+extension HomeViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello \(indexPath.item)"
+        return cell
+    }
+    
+    
 }
